60d30f5a530b0bd6a326d671d7aa466a00e51258
gregr <greg.weiqi@gmail.com>
2018-01-08 13:43:31 -0800

    Revise overview

--- overview.rkt
+++ overview.rkt
@@ -2,28 +2,10 @@
 (provide (all-defined-out))
 (require "mk.rkt")
 
-;(define (append l s)
-  ;(if (null? l)
-    ;s
-    ;(cons (car l) (append (cdr l) s))))
-
-;(define (append l s)
-  ;(cond
-    ;((null? l) s)
-    ;(else (cons (car l) (append (cdr l) s)))))
-
 (define (append l s)
-  (match l
-    ('() s)
-    (`(,a . ,d) (cons a (append d s)))))
-
-;(define (appendo l s ls)
-  ;(conde
-    ;((== '() l) (== s ls))
-    ;((fresh (a d ds)
-       ;(== `(,a . ,d) l)
-       ;(appendo d s ds)
-       ;(== `(,a . ,ds) ls)))))
+  (cond
+    ((null? l) s)
+    (else (cons (car l) (append (cdr l) s)))))
 
 (define (appendo l s ls)
   (conde
@@ -35,40 +17,18 @@
 
 
 (define (lookup x env)
-  (match-let ((`((,y . ,v) . ,rest) env))
-    (if (eqv? y x)
-      v
-      (lookup x rest))))
-
-;(define (lookup x env)
-  ;(match-let ((`((,y . ,v) . ,rest) env))
-    ;(cond
-      ;((eqv? y x) v)
-      ;(else (lookup x rest)))))
-
-(define (lookupo x env val)
-  (fresh (y v rest)
-    (== `((,y . ,v) . ,rest) env)
-    (conde
-      ((== y x) (== v val))
-      ((=/= y x) (lookupo x rest val)))))
-
-
-(define ((not-in-env? env) x)
   (match env
-    ('() #t)
     (`((,y . ,v) . ,rest)
-      (and (not (eqv? y x))
-           ((not-in-env? rest) x)))))
+      (cond
+        ((eqv? y x) v)
+        (else (lookup x rest))))))
 
-(define (not-in-envo x env)
-  (conde
-    ((== '() env))
-    ((fresh (y v rest)
-       (== `((,y . ,v) . ,rest) env)
-       (=/= y x)
-       (not-in-envo x rest)))))
-
+;(define ((not-in-env? env) x)
+  ;(match env
+    ;('() #t)
+    ;(`((,y . ,v) . ,rest)
+      ;(and (not (eqv? y x))
+           ;((not-in-env? rest) x)))))
 
 (define (evaluate expr env)
   (match expr
@@ -90,26 +50,20 @@
             (evaluate body `((,x . ,arg) . ,env^))))))))
 
 
-;(define (evaluate expr env)
-  ;(match expr
-    ;(`(,(? (not-in-env? env) quote) ,v)
-      ;v)
+(define (lookupo x env val)
+  (fresh (y v rest)
+    (== `((,y . ,v) . ,rest) env)
+    (conde
+      ((== y x) (== v val))
+      ((=/= y x) (lookupo x rest val)))))
 
-    ;((? symbol?)
-     ;(lookup expr env))
-
-    ;(`(,(? (not-in-env? env) 'cons) ,ea ,ed)
-      ;`(,(evaluate ea env) . ,(evaluate ed env)))
-
-    ;(`(,(? (not-in-env? env) 'lambda) (,(? symbol? x)) ,body)
-      ;`(closure ,x ,body ,env))
-
-    ;(`(,rator ,rand)
-      ;(match (evaluate rator env)
-        ;(`(closure ,x ,body ,env^)
-          ;(let ((arg (evaluate rand env)))
-            ;(evaluate body `((,x . ,arg) . ,env^))))))))
-
+(define (not-in-envo x env)
+  (conde
+    ((== '() env))
+    ((fresh (y v rest)
+       (== `((,y . ,v) . ,rest) env)
+       (=/= y x)
+       (not-in-envo x rest)))))
 
 (define (evaluateo expr env val)
   (conde
